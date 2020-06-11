c5aab338760332765623c40948127d4050536d23
chylinski.t <chylinski.t@outlook.com>
2020-06-10 23:54:52 -0400

    inital commit

---
+++ ucl_site.js
@@ -0,0 +1,26 @@
+
+
+
+const section_1 = [];
+const section_2 = ['Lazio','Athletico Madrid','Borussia Dortmund','Napoli'];
+const section_3 = ['Sevilla','Bayern Leverkuse','Real Sociedad','Hertha Berlin','Ajax','Lille','Roma','Atlanta'];
+const section_4 = ['AC Milan', 'Marseille','Valencia FC','Getafe','Porto','Red Bull Salzburg','Lyon','Monaco'];
+
+// console.log(section_1.length, ' show ')
+// console.log(section_2.length, ' show ')
+// console.log(section_3.length, ' show ')
+// console.log(section_4.length, ' show ')
+
+let groupA = [];
+let groupB = [];
+let groupC = [];
+let groupD = [];
+
+currentTeamB = section_2[Math.floor(Math.random() * section_2.length)];
+console.log(currentTeamB, ' team')
+
+
+//currentTeamA = section_1[Math.floor(Math.random() * section_1.length)];
+// currentTeamB = section_2[Math.floor(Math.random() * section_2.length)];
+// currentTeamC = section_3[Math.floor(Math.random() * section_3.length)];
+// currentTeamD = section_4[Math.floor(Math.random() * section_4.length)];
