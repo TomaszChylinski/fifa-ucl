5e4f31350c480f7f9afbd36598498bbe2e2e2321
chylinski.t <chylinski.t@outlook.com>
2020-06-11 00:32:08 -0400

    added tier 3 teams

--- ucl_site.js
+++ ucl_site.js
@@ -2,22 +2,18 @@
 
 
 const section_1 = [];
-const section_2 = ['Lazio','Napoli'];
-const section_3 = ['Sevilla','Bayern Leverkuse','Real Sociedad','Hertha Berlin','Ajax','Lille','Roma','Atlanta'];
+const section_2 = [];
+const section_3 = [];
 const section_4 = ['AC Milan', 'Marseille','Valencia FC','Getafe','Porto','Red Bull Salzburg','Lyon','Monaco'];
 
-// console.log(section_1.length, ' show ')
-// console.log(section_2.length, ' show ')
-// console.log(section_3.length, ' show ')
-// console.log(section_4.length, ' show ')
 
 let groupA = [];
 let groupB = [];
 let groupC = [];
 let groupD = [];
 
-currentTeamB = section_2[Math.floor(Math.random() * section_2.length)];
-console.log(currentTeamB, ' team')
+currentTeamC = section_3[Math.floor(Math.random() * section_3.length)];
+console.log(currentTeamC, ' team')
 
 
 //currentTeamA = section_1[Math.floor(Math.random() * section_1.length)];
